ba991d69c0a324d11d4cfc07c802b95c1eaaba96
Maryam <maryam_barket123@hotmail.com>
2021-07-06 15:08:12 +0530

    last commit

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ let resumeButton = document.getElementById('resume-button')
 // HTML2PDF OPTIONS
 let opt = {
     margin:       0,
-    filename:     'myResume.pdf',
+    filename:     'myCV.pdf',
     image:        { type: 'jpeg', quality: 0.98 },
     html2canvas:  { scale: 4 },
     jsPDF:        { format: 'a4', orientation: 'portrait' }
